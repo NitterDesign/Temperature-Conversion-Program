2c4165dfc1f42e8f1a7f2d072507dd2783b939ac
Nitter Design <webmaster@nitter.com>
2024-10-08 15:31:26 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+// TEMPERATURE CONVERSION PROGRAM
+
+const textBox = document.getElementById("textBox");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelcius = document.getElementById("toCelcius");
+const result = document.getElementById("result");
+let temp;
+
+
+function convert() {
+    
+    if(toFahrenheit.checked){
+        // result.textContent = "You selected to Fahrenheit";
+        temp = Number(textBox.value);
+        temp = temp * 9 / 5 + 32;
+        result.textContent = temp.toFixed(1) + "°F";
+    }
+    else if(toCelcius.checked){
+        // result.textContent = "You selected to Celcius";
+        temp = Number(textBox.value);
+        temp = (temp - 32) * (5/9);
+        result.textContent = temp.toFixed(1) + "°C";
+    }
+    else{
+        result.textContent = "Select a unit!";
+    }
+}
